1bb7c845de3468a55786b362fde007c318f2c06a
angelineliuu <124648429+angelineliuu@users.noreply.github.com>
2023-05-01 23:26:31 -0700

    finished final

--- final/Assets/javascript.js
+++ final/Assets/javascript.js
@@ -33,6 +33,76 @@ $(document).ready(function() {
         text: "BCorp Certification Status",
         fontSize: 20,
         fontColor: "#20b965"},
+        legend: {display: false},
+}
+})})
+
+$(document).ready(function() {
+  var canvas = $("#Entity")
+  var xValues = ["Private","Non-Proft", "Educational", "Public", "Investors", "Self-Owned", "Unknown"];
+  var yValues = [104,26, 18,18,14,8,1];
+  var barColors = ["#373b67", "#373b67", "#373b67", "#373b67", "#373b67", "#373b67", "#373b67"];
+
+  var Entity = new Chart(canvas, {
+    type: "bar",  
+    data: {
+    labels: xValues,
+    datasets: [{
+        backgroundColor: barColors,
+        data: yValues,}]},
+    options: {
+        title: {
+        display: true,
+        text: "Entity Type",
+        fontSize: 20,
+        fontColor: "#20b965"},
+        legend: {display: false}
+}
+})})
+
+$(document).ready(function() {
+  var canvas = $("#Year")
+  var xValues = [1636,1831,1841,1861,1864,1866,1868,1872,1873,1885,1898,1902,1906,1911, 1914,1919,1924,1930,1935,1943,1945,1948,1953,1957,1958,1975,1976,1981,1982,1984,1985,1986,1987,1988,1989, 1991, 1992, 1993, 1994,1997,
+    1998,
+    1999,
+    2000,
+    2001,
+    2002,
+    2003,
+    2004,
+    2006,
+    2007,
+    2008,
+    2009,
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,];
+  var yValues = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1, 2, 1,1,1,1,3,2,3,1,3,1,1,2,2,3,2,4,4,1,4,1,3,1,2,6,4,7,5,6,13,7,11,13,7,11,14,9,1];
+  var barColors = new Array(63).fill('#20b965');
+
+  var Year = new Chart(canvas, {
+    type: "bar",  
+    data: {
+    labels: xValues,
+    datasets: [{
+        backgroundColor: barColors,
+        data: yValues,}]},
+    options: {
+        title: {
+        display: true,
+        text: "Year Founded",
+        fontSize: 20,
+        fontColor: "#20b965"},
         legend: {display: false}
 }
 })})
@@ -58,7 +128,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -84,7 +155,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -110,7 +182,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -136,7 +209,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -162,7 +236,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -188,7 +263,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
@@ -214,7 +290,8 @@ $(document).ready(function() {
         text: "Industry Distribution",
         fontSize: 20,
         fontColor: "#20b965"},
-        legend: {position:'right', maxHeight: 30}
+        legend: {position:'right', maxHeight: 30},
+        responsive: false
       }
     }
 )})
